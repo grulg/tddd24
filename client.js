2038496a21282c2d16c6528139e1d404cc364372
Alexander Haeger <me@haeger.me>
2014-01-27 17:28:34 +0100

    Step 3

--- client.js
+++ client.js
@@ -23,8 +23,24 @@ validateSignIn = function(formData) {
     return email.trim() != "" && password != "";
 };
 
-validateSignUp = function(formData) {
-    return true;
+submitSignUp = function(formData) {
+    console.log("Validating... ");
+    if(validateSignUp(formData)) {
+        console.log("Complete.");
+    } else {
+        console.log("User fucked up.");
+    }
 };
 
+validateSignUp = function(formData) {
+    var email = formData.email.value;
+    var password = formData.password.value;
+    var password2 = formData.password2.value;
+    var fname = formData.fname.value;
+    var lname = formData.lname.value;
+    var gender = formData.gender.value;
+    var city = formData.city.value;
+    var country = formData.country.value;
 
+    return (email != "") && (password != "") && (password2 != "") && (fname != "") && (lname != "") && (gender != "") && (city != "") && (password === password2);
+};
