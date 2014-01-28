711f676a2a01e5830ad7d7238df01633ea96edb2
Alexander Haeger <me@haeger.me>
2014-01-28 16:07:32 +0100

    Step 4

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     displayView();
+    defaultBorder = document.signUpForm.elements[0].style.border;
 };
 
 displayView = function() {
@@ -27,20 +28,66 @@ submitSignUp = function(formData) {
     console.log("Validating... ");
     if(validateSignUp(formData)) {
         console.log("Complete.");
+
+        var dataObject = new Object();
+        dataObject.email = formData.email.value;
+        dataObject.password = formData.password.value;
+        dataObject.firstname = formData.fname.value;
+        dataObject.familyname = formData.lname.value;
+        dataObject.gender = formData.gender.value;
+        dataObject.city = formData.city.value;
+        dataObject.country = formData.country.value;
+
+        var result = serverstub.signUp(dataObject);
+        document.getElementById("sign-up-message").innerHTML = result.message;
+
+        if(result.success) {
+            clearSignUpForm(formData);
+        } else {
+            formData.email.style.borderColor = "red"
+        }
+
     } else {
         console.log("User fucked up.");
     }
 };
 
 validateSignUp = function(formData) {
-    var email = formData.email.value;
-    var password = formData.password.value;
-    var password2 = formData.password2.value;
-    var fname = formData.fname.value;
-    var lname = formData.lname.value;
-    var gender = formData.gender.value;
-    var city = formData.city.value;
-    var country = formData.country.value;
 
-    return (email != "") && (password != "") && (password2 != "") && (fname != "") && (lname != "") && (gender != "") && (city != "") && (password === password2);
+    var okay = true;
+    var form = document.signUpForm;
+    for(i = 0; i < form.elements.length - 1 ; i++) {
+        if(form.elements[i].value == "") {
+            okay = false;
+            form.elements[i].style.borderColor = "red";
+        }
+    }
+
+    if(formData.password.value != formData.password2.value) {
+        okay = false;
+        formData.password.style.borderColor = "red";
+        formData.password2.style.borderColor = "red";
+    }
+
+    return okay;
 };
+
+clearSignUpForm = function(formData) {
+    formData.email.value = "";
+    formData.password.value = "";
+    formData.password2.value = "";
+    formData.fname.value = "";
+    formData.lname.value = "";
+    formData.gender.selectedIndex = 0;
+    formData.city.value = "";
+    formData.country.value = "";
+}
+
+clearErrorBorder = function(element) {
+    // TODO Prettier solution? defaultBorder is declared in onLoad...
+    element.style.border = defaultBorder;
+
+    if(element.name == "email") {
+        document.getElementById("sign-up-message").innerHTML = "";
+    }
+}
